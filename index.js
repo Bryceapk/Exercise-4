55f642ad08e8b3e1abab78c9354934b76b8ecbf5
Bryceapk <Bryceapk1@gmail.com>
2023-11-06 00:54:08 -0600

    Exercise 4

---
+++ index.js
@@ -0,0 +1,13 @@
+let payrate= 12.50
+let hrsworked= 45
+let grosspay= payrate * hrsworked
+
+if (hrsworked <=40 ) {
+    grosspay =payrate * hrsworked;
+} 
+
+else {
+    (hrsworked >40) 
+    grosspay =payrate * hrsworked * 1.5;
+}
+ console.log(grosspay)
